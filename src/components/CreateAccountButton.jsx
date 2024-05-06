04869c044104e34297713335b07df1dcd64bafe3
Lucas Hahne <lucas-hahne@web.de>
2024-05-06 22:08:00 +0200

    first commit

---
+++ src/components/CreateAccountButton.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const CreateAccountButton = () => {
+  return (
+    <a
+      href="#"
+      className="bg-gradient-to-r from-orange-500 to-red-800 hover:from-orange-600 hover:to-red-900 hover:transition-colors duration-500 py-2 px-3 rounded-md"
+    >
+      Create an account
+    </a>
+  );
+};
+
+export default CreateAccountButton;
---
+++ src/components/SignIn.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SignInButton = () => {
+  return (
+    <a href="#" className="py-2 px-3 border rounded-md">
+      Sign In
+    </a>
+  );
+};
+
+export default SignInButton;
